910b1b6d580f4e3c128b87df209752793e170443
sachin <sachin.kumar@trantorinc.com>
2020-03-17 18:52:36 +0530

    working on admin panel for parameters dependencies

--- resources/views/partials/admin/parameter/_deliveryservice.blade.php
+++ resources/views/partials/admin/parameter/_deliveryservice.blade.php
@@ -1,36 +1,42 @@
-<div class="card mb-4">
-                <div class="card-header"><i class="fas fa-table mr-1"></i>Delivery Service</div>
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                <thead>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Actions</th>
-                                    </tr>
-                                </thead>
-                                <tfoot>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Actions</th>
-                                    </tr>
-                                </tfoot>
-                                <tbody>
-                                    @foreach($deliveryservice as $service)
-                                    <tr>
-                                        <td>{{ $service->delivery_service }}</td>
-                                        <td>
-                                            <form method="GET">
-                                                <input type="submit" value="edit" >
-                                            </form>
-                                            <form method="GET">
-                                                <input type="submit" value="delete" >
-                                            </form>
-                                        </td>
-                                    </tr> 
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+<div class="card mb-4 mt-4">
+    <div class="card-header"><span>Delivery Service</span>
+
+        <a href="#" class="btn btn-primary float-right ">
+            <span>Create New Delivery Service</span>
+        </a>
+    </div>
+
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th>Name</th>
+                        <th>Actions</th>
+                    </tr>
+                </tfoot>
+                <tbody>
+                    @foreach($deliveryservice as $service)
+                    <tr>
+                        <td>{{ $service->delivery_service }}</td>
+                        <td>
+                        <a href="#" class="btn btn-success">
+                                <span>Edit</span>
+                            </a>
+                            <a href="#" class="btn btn-danger">
+                                <span>Delete</span>
+                            </a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
