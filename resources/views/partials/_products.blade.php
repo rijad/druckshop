3f4b40108c3aa2f4f2539d825650c28ef899a355
swatibatra01 <batraswati01@gmail.com>
2020-04-13 19:37:41 +0530

    resolved designing issues

--- resources/views/partials/_products.blade.php
+++ resources/views/partials/_products.blade.php
@@ -7,72 +7,85 @@
 				<div class="col-row mb-5"> 
 
 					<div class="product-item-img col-half text-left">
-						<span class="img-back"><img src="{{ asset($listing->image_path)}}" alt="" /></span>
-					</div>
-					<div class="product-item-img col-half text-left">
-						<span class="img-back">
-							@foreach ($listing->psProductImages as $image)
-							<img src="{{ asset($listing->image_path)}}" alt="" height="50" width="50" /></span>
-							@endforeach
-						</div>
+						 <span class="img-back"><img src="{{ asset($listing->image_path)}}" alt="" /></span>
 
-						<div class="product-item col-half">
-							<?php $locale = session()->get('locale'); 
-
-							if ($locale == 'gr') { ?>
-
-							<h2>{{$listing->title_german}}</h2>	
-							<p>{{$listing->short_description_german}}</p>
-							<?php } else { ?>
-
-							<h2>{{$listing->title_english}}</h2>	
-							<p>{{$listing->short_description_english}}</p>
-
-							<?php } ?>
-
-							<a href="{{ route($listing->product_page_url,['id'=>$listing->id])}}" class="btn-gray">{{ trans('product.to_order')}}</a>
-							<a href="{{ route('product-information') }}#{{$listing->title_english}}">{{ trans('product.product_info')}}</a>
-						</div> 
-
-					</div><!-- col row ends -->  
-
-					@else
-
-					<div class="col-row mb-5"> 
-
-						<div class="product-item-img col-half text-right pull-right">
-							<span class="img-back"><img src="{{ asset($listing->image_path)}}" alt="" /></span>
-						</div>
-						<div class="product-item-img col-half text-left">
+						 <div class="rv-imagelist">
 							<span class="img-back">
-								@foreach ($listing->psProductImages as $image)
-								<img src="{{ asset($listing->image_path)}}" alt="" height="50" width="50" /></span>
-								@endforeach
-							</div>
+							 @foreach ($listing->psProductImages as $image)
+							 	<img src="{{ asset($listing->image_path)}}" alt="" height="50" width="50" />
+							 @endforeach
+							 </span>
+						</div>	 
+						
+					</div>
+					<!-- <div class="product-item-img col-half text-left">
+						 <span class="img-back">
+						 @foreach ($listing->psProductImages as $image)
+						 	<img src="{{ asset($listing->image_path)}}" alt="" height="50" width="50" /></span>
+						 @endforeach
+					</div> -->
 
-							<div class="product-item col-half">
-								
-								<?php $locale = session()->get('locale'); 
-								
-								if ($locale == 'gr') { ?>
+					<div class="product-item col-half">
+						<h2>{{$listing->title_english}}</h2>	
+						<p>{{$listing->short_description_english}}</p>
+						 
+						<a href="{{ route($listing->product_page_url) }}" class="btn-gray">To Order</a>
+						<a href="{{ route('product-information') }}#{{$listing->title_english}}">product Info</a>
+					</div> 
+					
+				</div><!-- col row ends -->
 
-								<h2>{{$listing->title_german}}</h2>	
-								<p>{{$listing->short_description_german}}</p>
-								<?php } else { ?>
+				@else
 
-								<h2>{{$listing->title_english}}</h2>	
-								<p>{{$listing->short_description_english}}</p>
+				<div class="col-row mb-5"> 
 
-								<?php } ?>
+					<div class="product-item-img col-half text-right pull-right">
+						<span class="img-back"><img src="{{ asset($listing->image_path)}}" alt="" /></span>
 
-								<a href="{{ route($listing->product_page_url,['id'=>$listing->id])}}" class="btn-gray">{{ trans('product.to_order')}}</a>
-								<a href="{{ route('product-information') }}#{{$listing->title_english}}">{{ trans('product.product_info')}}</a>
-							</div> 
+						<div class="rv-imagelist">
+							<span class="img-back">
+							 @foreach ($listing->psProductImages as $image)
+							 	<img src="{{ asset($listing->image_path)}}" alt="" height="50" width="50" />
+							 @endforeach
+							 </span>
+						</div>	 
 
-						</div><!-- col row ends --> 
+					</div>
+					<!-- <div class="product-item-img col-half text-left">
+						 <span class="img-back">
+						 @foreach ($listing->psProductImages as $image)
+						 	<img src="{{ asset($listing->image_path)}}" alt="" height="50" width="50" /></span>
+						 @endforeach
+					</div> -->
 
-						@endif
+					<div class="product-item col-half">
+						<h2>{{$listing->title_english}}</h2>	
+						<p>{{$listing->short_description_english}}</p>
+						
+						<a href="{{ route($listing->product_page_url) }}" class="btn-gray">To Order</a>
+						<a href="{{ route('product-information') }}#{{$listing->title_english}}">product Info</a>
+					</div> 
 
-						@endforeach
+				</div><!-- col row ends --> 
 
-					</div>		 
+				@endif
+
+				@endforeach
+
+				<div class="col-row mb-5"> 
+
+					<div class="product-item-img col-half text-right pull-right">
+						<span class="img-back"><img src="{{ asset('/public/images/product4.jpg')}}" alt="" /></span>
+					</div>
+
+					<div class="product-item col-half">
+						<h2>Sample with 15 free pages</h2>	
+						<p>You just want to print out your work? No problem, with us you can even print and have your work tied up elsewhere.</p>
+						 
+						<a href="{{ route('free-sample') }}" class="btn-gray">To Order</a>
+						<a href="{{ route('product-information') }}#{{$listing->title_english}}">product Info</a>
+					</div> 
+
+				</div><!-- col row ends --> 
+
+		</div>		 
