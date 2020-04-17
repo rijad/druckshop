8f646acd10412fb424c4ebd6383b5cd008f597e7
sachin-trantor <sachin.kumar@trantorinc.com>
2020-04-17 19:21:37 +0530

    worked on email template integration

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,131 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Druckshop Welcome</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+
+
+    <table width="100%" cellspacing="0" cellpadding="0" border="0">
+        <tbody>
+            <tr>
+                <td width="100%" valign="top" bgcolor="#ffffff" align="left">
+                    <div align="center">
+                        <table style="border:1px solid #dae1e7;" width="600" cellspacing="0" cellpadding="0" border="0" align="center">
+                            <tbody>
+                                <tr>
+                                    <td valign="top" align="left">
+                                        <table width="100%" cellspacing="0" cellpadding="0" border="0">
+                                            <tbody>
+                                                <tr>
+                                                    <td style="padding-top:30px;padding-right:30px;padding-bottom:30px;padding-left:30px" valign="top" bgcolor="#ffffff" align="left">
+                                                        <table width="100%" cellspacing="0" cellpadding="0" border="0">
+                                                            <tbody>
+
+                                                                <tr>
+                                                                    <td valign="top" align="left">
+                                                                        <table cellspacing="0" cellpadding="0" border="0" width="100%">
+                                                                            <tbody>
+                                                                                <tr>
+                                                                                    <td style="padding-bottom:24px;">
+                                                                                        <table width="100%">
+                                                                                            <tbody>
+                                                                                                <tr align="center">
+                                                                                                    <td>
+                                                                                                        <a href="{{ @$base_url }}" target="_blank"><img src="{{ @$logo_url }}" alt="Logo" style="width: 400px;"></a>
+                                                                                                    </td>
+                                                                                                </tr>
+                                                                                                <tr>
+                                                                                                    <td style="font-family:Arial,Helvetica,sans-serif;font-size:22px;color:#1d2b4c;line-height:24px;padding-top:30px;text-align: center;">
+                                                                                                        Abschlussarbeit-biden.de
+                                                                                                    </td>
+                                                                                                </tr>
+                                                                                                <tr>
+                                                                                                    <td style="font-family:Arial,Helvetica,sans-serif;font-size:22px;color:#1d2b4c;line-height:24px;padding-bottom:24px;text-align: center;">
+                                                                                                        By {{ @$name }}
+                                                                                                    </td>
+                                                                                                </tr>
+                                                                                            </tbody>
+                                                                                        </table>
+                                                                                    </td>
+                                                                                </tr> 
+                                                                            </tbody>
+                                                                        </table>
+                                                                        <table cellspacing="0" cellpadding="0" border="0">
+                                                                            <tbody>
+                                                                                <tr>
+                                                                                    <td style="font-family:Arial,Helvetica,sans-serif;font-size:18px;color:#B88862;line-height:10px" valign="top" align="left">Website Description</td>
+                                                                                </tr>
+                                                                                <tr>
+                                                                                    <td style="font-family:Arial,Helvetica,sans-serif;font-size:14px;color:#606470;line-height:24px;padding-bottom:10px;padding-top: 10px;" valign="top" align="left"><b>Abschlussarbeit-biden.de</b> sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</td>
+                                                                                </tr>
+                                                                                <tr>
+                                                                                    <td style="font-family:Arial,Helvetica,sans-serif;font-size:14px;color:#606470;line-height:24px;padding-bottom:10px;padding-top: 10px;" valign="top" align="left">  
+                                                                                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+                                                                                    </td>
+                                                                                </tr>  
+                                                                                <tr>
+                                                                                    <td>
+                                                                                        <tr><td style="font-family:Arial,Helvetica,sans-serif;font-size:14px;color:#606470;line-height:24px;padding-top: 10px;" valign="top" align="left"> - At vero eos et accusamus et iusto odio dignissimos ducimus </td></tr>
+                                                                                        <tr><td style="font-family:Arial,Helvetica,sans-serif;font-size:14px;color:#606470;line-height:24px;padding-top: 10px;" valign="top" align="left"> - qui blanditiis praesentium voluptatum deleniti atque corrupti</td></tr>
+                                                                                        <tr><td style="font-family:Arial,Helvetica,sans-serif;font-size:14px;color:#606470;line-height:24px;padding-top: 10px;" valign="top" align="left"> - similique sunt in culpa qui officia deserunt mollitia animi.</td></tr>
+                                                                                        <tr><td style="font-family:Arial,Helvetica,sans-serif;font-size:14px;color:#606470;line-height:24px;padding-top: 10px;" valign="top" align="left"> - Eos et accusamus et iusto odio dignissimos ducimus sunt</td></tr>
+                                                                                        <tr><td style="font-family:Arial,Helvetica,sans-serif;font-size:14px;color:#606470;line-height:24px;padding-top: 10px;" valign="top" align="left"> - Blanditiis praesentium voluptatum deleniti et accusamus et iusto odio</td></tr>
+                                                                                    </td>
+
+                                                                                </tr>   
+                                                                                
+
+
+                                                                                <tr>
+                                                                                    <td valign="top">
+                                                                                        <table width="100%" cellspacing="0" cellpadding="0" border="0">
+                                                                                            <tbody>
+                                                                                                <tr>
+                                                                                                    <td style="text-align:center;font-family:Arial,Helvetica,sans-serif;font-size:12px;line-height:18px;font-weight:400;color:#b88862;" valign="top" align="left">
+                                                                                                        Copyright © 2020 ABSCHLUSSARBEIT-BIDEN.DE Inc. All rights reserved.
+                                                                                                        <br>
+                                                                                                    </td>
+                                                                                                </tr>
+                                                                                            </tbody>
+                                                                                        </table>
+                                                                                    </td>
+                                                                                </tr>
+
+                                                                            </tbody>
+                                                                        </table>
+                                                                    </td>
+                                                                </tr>
+
+
+
+
+
+
+
+                                                            </tbody>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+
+
+
+
+                            </tbody>
+                        </table>
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+
+</body>
+</html>
